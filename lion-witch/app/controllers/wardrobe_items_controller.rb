f64ef9567b20cb7bf4f21ba60974e896b7e81ebe
MakerSquare Student <students@themakersquare.com>
2013-09-30 10:31:19 -0500

    Fix failing test by adding index method.

--- lion-witch/app/controllers/wardrobe_items_controller.rb
+++ lion-witch/app/controllers/wardrobe_items_controller.rb
@@ -1,4 +1,5 @@
 class WardrobeItemsController < ApplicationController
   def index
+    @wardrobe_items = WardrobeItem.all
   end
 end
--- lion-witch/spec/controllers/wardrobe_items_controller_spec.rb
+++ lion-witch/spec/controllers/wardrobe_items_controller_spec.rb
@@ -5,6 +5,8 @@ describe WardrobeItemsController do
     it "assigns all wardrobe items as @wardrobe_items" do
       wardrobe_item = WardrobeItem.create
       get :index
+
+      expect(assigns('wardrobe_items')).to eq ([wardrobe_item])
     end
   end
 end
